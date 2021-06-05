91892d9bcf59f1bc7394edfaaf670c24f3ea2f5a
shandelier <26363920+Shandelier@users.noreply.github.com>
2021-06-05 17:36:36 +0200

    cleanup

--- run.sh
+++
@@ -1,4 +0,0 @@
-python ./main.py --pose 0 --input 'input\Straight-Synthetic-HDR-26s'
-python ./main.py --pose 1 --input 'input\Slouch-Synthetic-HDR-26s'
-python ./main.py --pose 0 --input 'input\Straight-Synthetic-noHDR-26s'
-python ./main.py --pose 1 --input 'input\Slouch-Synthetic-noHDR-26s'
